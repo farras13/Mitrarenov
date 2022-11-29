b5c0d5e547f9e1624846e137aca130d72b878841
farissyf30 <42666064+farissyf30@users.noreply.github.com>
2022-11-29 14:24:46 +0700

    order

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      * @var string
      */
     // public $baseURL = 'https://mitrarenov.soldig.co.id/';
-    public $baseURL = 'http://localhost/mitrarenov/public';
+    public $baseURL = 'http://localhost/mitrarenov';
 
     /**
      * --------------------------------------------------------------------------
